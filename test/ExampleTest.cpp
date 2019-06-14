cfa4c24612385597e09800ab8bf55b5c4828bea2
Laykel <layke.luc@gmail.com>
2019-06-14 10:39:34 +0200

    Fix setup for google test

---
+++ test/ExampleTest.cpp
@@ -0,0 +1,13 @@
+//
+// Created by luc on 29.05.19.
+//
+
+#include "gtest/gtest.h"
+#include "../src/Customer.h"
+
+TEST(ExampleTest, simple) {
+   // Code
+   Customer cust("Violette");
+
+   ASSERT_EQ(cust.getName(), "Violette");
+}
