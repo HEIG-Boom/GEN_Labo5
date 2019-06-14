bd958f9b81d3d82220c2460a661f4ecd846211da
Laykel <layke.luc@gmail.com>
2019-06-14 14:58:46 +0200

    Working unit test for Customer.statement() function

--- test/CustomerTest.cpp
+++ test/CustomerTest.cpp
@@ -5,9 +5,11 @@
 #include "gtest/gtest.h"
 #include "../src/Customer.h"
 
+// Integration test for the Customer.statement() member function
 TEST(ExampleTest, simple) {
-   // Code
    Customer cust("Violette");
 
-   ASSERT_EQ(cust.getName(), "Violette");
+   cust.addRental(Rental(Movie("Borat"), 4));
+
+   ASSERT_EQ(cust.statement(), "Rental Record for Violette\n\tBorat\t5\nAmount owed is 5\nYou earned 1 frequent renter points");
 }
