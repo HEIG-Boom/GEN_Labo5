1bd199ad86d26da59ef6eee0f9bd688ae95da563
Mateo Tutic <mateo@tutic.ch>
2019-06-14 15:32:18 +0200

    Rental code style

--- src/Rental.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by luc on 14.06.19.
-//
-
--- src/Rental.h
+++ src/Rental.h
@@ -1,29 +1,32 @@
 // Rental.h
 #ifndef RENTAL_H
 #define RENTAL_H
+
 #include "Movie.h"
 
 class Rental {
 public:
-    Rental( const Movie& movie, int daysRented );
+   Rental(const Movie& movie, int daysRented);
 
-    int getDaysRented() const;
-    const Movie& getMovie() const;
+   int getDaysRented() const;
+
+   const Movie& getMovie() const;
 
 private:
-    Movie _movie;
-    int _daysRented;
+   Movie _movie;
+   int _daysRented;
 };
 
-inline Rental::
-Rental( const Movie& movie, int daysRented )
-        : _movie( movie )
-        , _daysRented( daysRented ) {}
+inline Rental::Rental(const Movie& movie, int daysRented)
+      : _movie(movie),
+        _daysRented(daysRented) {}
 
-inline int Rental::
-getDaysRented() const { return _daysRented; }
+inline int Rental::getDaysRented() const {
+   return _daysRented;
+}
 
-inline const Movie& Rental::
-getMovie() const { return _movie; }
+inline const Movie& Rental::getMovie() const {
+   return _movie;
+}
 
 #endif // RENTAL_H
